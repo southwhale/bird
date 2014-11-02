4343c33423900a4f69dff1b020f3d8d97740350c
iamweilee <383523223@qq.com>
2014-11-03 01:02:52 +0800

    no message

--- dep/lib/bird/base/bird.observer.js
+++
@@ -1,4 +0,0 @@
-define("bird.observer", [ "./bird.__observer__" ], function(require) {
-    var Observer = require("./bird.__observer__");
-    return new Observer();
-});
--- dep/lib/bird/mvvm/bird.router.js
+++
@@ -1,3 +0,0 @@
-define("bird.router", [ "./bird.router.pushstate", "./bird.router.hashchange" ], function(require) {
-    return history.pushState ? require("./bird.router.pushstate") : require("./bird.router.hashchange");
-});
