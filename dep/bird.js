292a33681ff566085a44fdd8f7273b546e683d81
weilee <383523223@qq.com>
2015-01-15 10:47:27 +0800

    更新bird.validator，添加几种常用正则，特别是身份证号的验证规则

--- dep/bird.js
+++ dep/bird.js
@@ -6575,23 +6575,32 @@ define("bird.validator", [ "bird.lang", "bird.string", "bird.array", "bird.objec
     var dom = require("bird.dom");
     function Validator() {}
     (function() {
-        //var messageStack = [];
-        /*var messageMap = {
-			'required': '请输入',
-			'number': '只能输入数字',
-			'positiveNumber': "只能输入正数",
-			'positiveInt': "只能输入正整数",
-			'negativeNumber': "只能输入负数",
-			'negativeInt': "只能输入负整数",
-			'notPositiveNumber': "只能输入非正数",
-			'notPositiveInt': "只能输入非正整数",
-			'notNegativeNumber': "只能输入非负数",
-			'notNegativeInt': "只能输入非负整数",
-			'email': '邮箱格式不正确',
-			'mobile': '手机号码格式不正确',
-			'idNumber': '身份证号码格式不正确',
-			'float': '小数位不能超过{{digit}}位'
-		};*/
+        var checkReg = {
+            require: /.+/,
+            email: /^\w+[-+.\w]*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/,
+            phone: /^((\(\d{3}\))|(\d{3}\-))?(\(0\d{2,3}\)|0\d{2,3}-)?[1-9]\d{6,7}$/,
+            mobile: /^0?1\d{10}$/,
+            name: /^[\u0391-\uFFE5]{2,20}$/,
+            pinyin: /^[a-zA-Z]*$/,
+            url: /^http:\/\/[A-Za-z0-9]+\.[A-Za-z0-9]+[\/=\?%\-&_~`@[\]\':+!]*([^<>\"\"])*$/,
+            idCard: /^\d{17}[A-Za-z0-9]$/,
+            currency: /^\d+(\.\d+)?$/,
+            number: /^\d+$/,
+            zip: /^\d{6}$/,
+            qq: /^[1-9]\d{4,11}$/,
+            Integer: /^[-\+]?\d+$/,
+            Double: /^[-\+]?\d+(\.\d+)?$/,
+            english: /^[A-Za-z]+$/,
+            chinese: /^[\u0391-\uFFE5]+$/,
+            passport: /^[Pp]([0-9]{7})$|^[Gg]([0-9]{8})$/,
+            address: /((.*[\u0391-\uFFE5].*)+){6,}/,
+            authCode: /^\d{6}$/,
+            date: /^\d{4}-\d{1,2}-\d{1,2}$/,
+            datetime: /^\d{4}-\d{1,2}-\d{1,2} \d{1,2}:\d{1,2}:\d{1,2}$/,
+            time: /^\d{1,2}:\d{1,2}:\d{1,2}$/,
+            carNo: /^[\u4e00-\u9fa5]{1}[a-zA-Z]{1}[a-zA-Z_0-9]{4}[a-zA-Z_0-9_\u4e00-\u9fa5]$|^[a-zA-Z]{2}\d{7}$/,
+            validCode: /^[A-Za-z0-9]{4}$/
+        };
         var me = this;
         var ruleMap = {
             required: {
@@ -6776,7 +6785,7 @@ define("bird.validator", [ "bird.lang", "bird.string", "bird.array", "bird.objec
                     if (value == null || value === "") {
                         return 0;
                     }
-                    if (/^1\d{10,11}$/.test(value)) {
+                    if (checkReg.mobile.test(value)) {
                         return 0;
                     }
                     return 1;
@@ -6785,15 +6794,41 @@ define("bird.validator", [ "bird.lang", "bird.string", "bird.array", "bird.objec
                     1: "手机号码格式不正确"
                 }
             },
-            idNumber: {
-                validate: function(value) {
-                    if (value == null || value === "") {
+            idCard: {
+                validate: function(b) {
+                    if (!b) {
                         return 0;
                     }
-                    if (/^(?:\d{15}|\d{18})$/.test(value)) {
-                        return 0;
+                    var a = checkReg.idCard;
+                    if (!a.test(b)) {
+                        return 1;
                     }
-                    return 1;
+                    var g = "11,12,13,14,15,21,22,23,31,32,33,34,35,36,37,41,42,43,44,45,46,50,51,52,53,54,61,62,63,64,65,71,81,82,91";
+                    var e = 0;
+                    var c = "";
+                    var f = b.length;
+                    if (!/^\d{17}(\d|x)$/i.test(b)) {
+                        return 1;
+                    }
+                    var l = b.replace(/x$/i, "a");
+                    var j = l.substr(0, 2);
+                    if (!(g.indexOf(j) >= 0)) {
+                        return 1;
+                    }
+                    if (f == 18) {
+                        sBirthday = l.substr(6, 4) + "-" + Number(l.substr(10, 2)) + "-" + Number(l.substr(12, 2));
+                        var k = new Date(sBirthday.replace(/-/g, "/"));
+                        if (sBirthday != k.getFullYear() + "-" + (k.getMonth() + 1) + "-" + k.getDate()) {
+                            return 1;
+                        }
+                        for (var h = 17; h >= 0; h--) {
+                            e += Math.pow(2, h) % 11 * parseInt(l.charAt(17 - h), 11);
+                        }
+                        if (e % 11 != 1) {
+                            return 1;
+                        }
+                    }
+                    return 0;
                 },
                 messageMap: {
                     1: "身份证号码格式不正确"
