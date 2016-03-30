27c5b26f8c9a08600c55c2fcc032d872b4a9c950
weilee <383523223@qq.com>
2016-03-30 17:53:52 +0800

    更新lang.debounce的实现

--- dep/lib/bird/base/bird.lang.js
+++ dep/lib/bird/base/bird.lang.js
@@ -262,18 +262,39 @@ define("bird.lang", [], function(require) {
             }
             return ctx;
         };
+        this.now = function() {
+            return Date.now ? Date.now() : new Date().getTime();
+        };
         this.debounce = function(func, wait, immediate) {
-            var timeout;
-            return function() {
-                var context = this, args = arguments;
-                var later = function() {
+            // immediate默认为false
+            var timeout, args, context, timestamp, result;
+            var me = this;
+            var later = function() {
+                // 当wait指定的时间间隔期间多次调用_.debounce返回的函数，则会不断更新timestamp的值，导致last < wait && last >= 0一直为true，从而不断启动新的计时器延时执行func
+                var last = me.now() - timestamp;
+                if (last < wait && last >= 0) {
+                    timeout = setTimeout(later, wait - last);
+                } else {
                     timeout = null;
-                    if (!immediate) func.apply(context, args);
-                };
+                    if (!immediate) {
+                        result = func.apply(context, args);
+                        if (!timeout) context = args = null;
+                    }
+                }
+            };
+            return function() {
+                context = this;
+                args = arguments;
+                timestamp = me.now();
+                // 第一次调用该方法时，且immediate为true，则调用func函数
                 var callNow = immediate && !timeout;
-                clearTimeout(timeout);
-                timeout = setTimeout(later, wait);
-                if (callNow) func.apply(context, args);
+                // 在wait指定的时间间隔内首次调用该方法，则启动计时器定时调用func函数
+                if (!timeout) timeout = setTimeout(later, wait);
+                if (callNow) {
+                    result = func.apply(context, args);
+                    context = args = null;
+                }
+                return result;
             };
         };
         this.noop = noop;
