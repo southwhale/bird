3bb36bbbb9601ea178907f2764007f3b0aa6f916
weilee <383523223@qq.com>
2016-08-16 02:45:04 +0800

    打个bind补丁

--- src/lib/bird/base/bird.syspatch.js
+++ src/lib/bird/base/bird.syspatch.js
@@ -20,6 +20,30 @@ define(function(require) {
 	}
 
 	/**
+	 * 增强bind实现
+	 */
+ 	!Function.prototype.bind && (Function.prototype.bind = function (oThis) {
+    if (typeof this !== "function") {
+      throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+    }
+
+    var aArgs = Array.prototype.slice.call(arguments, 1), 
+        fToBind = this, 
+        fNOP = function () {},
+        fBound = function () {
+          return fToBind.apply(
+              this instanceof fNOP && oThis ? this : oThis || window,
+              aArgs.concat(Array.prototype.slice.call(arguments))
+          );
+        };
+
+    fNOP.prototype = this.prototype;
+    fBound.prototype = new fNOP();
+
+    return fBound;
+  });
+
+	/**
 	 * 保证console在语法上可行
 	 */
 	if (ctx.DEBUG && typeof ctx.console === 'undefined') {
